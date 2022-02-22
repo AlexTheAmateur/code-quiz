2d054ac5ad508b701bb7e8c8e78a3e52588d0b9f
Alex Wu <awu4921@gmail.com>
2022-02-22 15:47:08 -0600

    finished creating highscore storage and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,214 +1,222 @@
 var body = document.body;
 var start = document.querySelector(".start-button");
+var scoreBoard = document.getElementById("scoreBoard");
 var timerEl = document.getElementById("timer");
 var timeLeft = 50;
 var questionNum = 0;
 var score = 0;
+var scores = [];
 
 const questionArray = [
   {
-    question: "sample text",
+    question: "An HTML document can contain ___",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text2",
-    answers: {
-      a: "answer2 1",
-      b: "answer2 2",
-      c: "answer2 3",
-      d: "answer2 4",
+      a: "Attributes",
+      b: "Tags",
+      c: "Raw text",
+      d: "All of the above",
     },
     correctAnswer: "d",
   },
   {
-    question: "sample text3",
+    question: "a page designed in HTML is called",
     answers: {
-      a: "answer3 1",
-      b: "answer3 2",
-      c: "answer3 3",
-      d: "answer3 4",
+      a: "Application",
+      b: "Cover page",
+      c: "Front-end",
+      d: "Web Page",
     },
     correctAnswer: "d",
   },
   {
-    question: "sample text",
+    question: "The HTML document contains a root tag called ___",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "HEAD",
+      b: "Title",
+      c: "Body",
+      d: "HTML",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question:
+      "If we want to place text around an image, which CSS property should we use?",
+    answers: {
+      a: "push",
+      b: "float",
+      c: "align",
+      d: "wrap",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Choose the correct HTML tag for a large title",
+    answers: {
+      a: "H1",
+      b: "Heading",
+      c: "Head",
+      d: "H6",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "In HTML, Uniform Resource Locator (URL) is used ___",
+    answers: {
+      a: "To create a frame document",
+      b: "To create an image map in a web page",
+      c: "To customize the image in a webpage",
+      d: "To identify a name or a resuorce on the internet",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "CSS is an acronym for ___",
+    answers: {
+      a: "Cascading Style Sheet",
+      b: "Custom Style Sheet",
+      c: "Cascading System Style",
+      d: "None of the above",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "What attribute can be added to many HTML/XHTML elements to identify them as a member of a specific group?",
+    answers: {
+      a: "Id",
+      b: "Class",
+      c: "Div",
+      d: "Span",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Which organization defines Web standards?",
+    answers: {
+      a: "Apple Inc.",
+      b: "IBM Corperation",
+      c: "World Wide Web Consortium",
+      d: "Microsoft Corperation",
     },
     correctAnswer: "c",
   },
   {
-    question: "sample text",
+    question: "HTML is considered as a ___",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "Programming language",
+      b: "OOP language",
+      c: "Markup language",
+      d: "High level language",
     },
     correctAnswer: "c",
   },
   {
-    question: "sample text",
+    question: "HTML was first proposed in ___",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "1980",
+      b: "1990",
+      c: "1995",
+      d: "2000",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Which of the following is not a browser?",
+    answers: {
+      a: "Mozilla Firefox",
+      b: "Netscape",
+      c: "Microsoft Bing",
+      d: "Opera",
     },
     correctAnswer: "c",
   },
   {
-    question: "sample text",
+    question: "Who is the main author of the HTML?",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "Brendan Eich",
+      b: "Tim Berners-Lee",
+      c: "Web developer",
+      d: "Google Inc",
     },
-    correctAnswer: "c",
+    correctAnswer: "b",
   },
   {
-    question: "sample text",
+    question:
+      "If we want to set the style for just one element, which css selector will we use?",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "id",
+      b: "text",
+      c: "class",
+      d: "name",
     },
-    correctAnswer: "c",
+    correctAnswer: "a",
+  },
+  //   {
+  //     question: "Which tag is used to list items with bullets?",
+  //     answers: {
+  //       a: "<bullet>",
+  //       b: "<list>",
+  //       c: "<ul>",
+  //       d: "<ol>",
+  //     },
+  //     correctAnswer: "c",
+  //   },
+  //   {
+  //     question: "How would you define a background image for a web page?",
+  //     answers: {
+  //       a: "<body background = 'test.jpg'>",
+  //       b: "<body background image = 'test.jpg'>",
+  //       c: "<background = 'test.jpg'>",
+  //       d: "<background image = 'test.jpg'>",
+  //     },
+  //     correctAnswer: "a",
+  //   },
+  {
+    question: "The first page of a website is typically called a ___",
+    answers: {
+      a: "Design page",
+      b: "Home page",
+      c: "Front page",
+      d: "Main page",
+    },
+    correctAnswer: "b",
   },
   {
-    question: "sample text",
+    question: "which is the correct syntax for the following CSS code?",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "Body:color=black",
+      b: "{body;color:black}",
+      c: "{body:color=black(body)}",
+      d: "Body{color:black}",
     },
-    correctAnswer: "c",
+    correctAnswer: "d",
   },
+  //   {
+  //     question: "What is the alternative to the <b> tag to make the text bold?",
+  //     answers: {
+  //       a: "<bold>",
+  //       b: "<strong>",
+  //       c: "<i>",
+  //       d: "<emp>",
+  //     },
+  //     correctAnswer: "b",
+  //   },
   {
-    question: "sample text",
+    question: "What does HTML stand for?",
     answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
+      a: "HyperText Markup Language",
+      b: "Hyper Teach Markup Language",
+      c: "Hyper Tech Markup Language",
+      d: "None of the above",
     },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "sample text",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4",
-    },
-    correctAnswer: "c",
+    correctAnswer: "a",
   },
 ];
 
 var startFunction = function () {
+  questionNum = 0;
+  score = 0;
+  timeLeft = 50;
   newQuestion();
   countdown();
 };
@@ -252,7 +260,6 @@ var answerQuestion = function () {
     event.target.id === questionArray[questionNum].correctAnswer &&
     event.target.className === "answer-button"
   ) {
-    score++;
     console.log("correct");
     questionNum++;
     score++;
@@ -264,8 +271,13 @@ var answerQuestion = function () {
     console.log("incorrect");
     questionNum++;
   }
-  newQuestion();
-  console.log(questionNum);
+  if (questionNum >= 17) {
+    submitScores();
+    return;
+  }
+  if (event.target.className === "answer-button") {
+    newQuestion();
+  }
 };
 
 var countdown = function () {
@@ -281,10 +293,80 @@ var countdown = function () {
     } else {
       timerEl.textContent = "";
       clearInterval(timeInterval);
+      submitScores();
     }
   }, 1000);
 };
 
+var highscores = function () {
+  var initialPage = document.querySelector("main");
+  initialPage.remove();
+  var main = document.createElement("main");
+  body.appendChild(main);
+  var startButton = document.createElement("button");
+  startButton.className = "start-button";
+  startButton.innerHTML = "Start Quiz";
+  main.appendChild(startButton);
+
+  var savedScores = localStorage.getItem("user");
+  savedScores = JSON.parse(savedScores);
+  for (var i = 0; i < savedScores.length; i++) {
+    var scorer = document.createElement("div");
+    scorer.innerHTML = savedScores[i].name + ": " + savedScores[i].score;
+    main.appendChild(scorer);
+  }
+
+  startButton.addEventListener("click", startFunction);
+};
+
+var submitScores = function () {
+  var initialPage = document.querySelector("main");
+  initialPage.remove();
+  var main = document.createElement("main");
+  body.appendChild(main);
+  var done = document.createElement("h1");
+  var thisScore = document.createElement("h2");
+  var inputName = document.createElement("input");
+  var submit = document.createElement("button");
+
+  inputName.type = "text";
+  inputName.name = "name";
+  inputName.id = "name";
+  submit.id = "submit";
+  submit.innerHTML = "submit";
+  done.innerHTML = "Finished!";
+  thisScore.innerHTML =
+    "Your score is " +
+    score +
+    " add a name below to be entered into the highscores!";
+
+  main.appendChild(done);
+  main.appendChild(thisScore);
+  main.appendChild(inputName);
+  main.appendChild(submit);
+  submit.addEventListener("click", saveToStorage);
+};
+
+var saveToStorage = function () {
+  var nameInput = document.querySelector("#name");
+  var user = {
+    name: nameInput.value.trim(),
+    score: score,
+  };
+  scores = JSON.parse(localStorage.getItem("user"));
+  scores.push(user);
+  localStorage.setItem("user", JSON.stringify(scores));
+
+  highscores();
+};
+
+var answerBoolean = function () {
+  if (event.target.className === "answer-button") {
+    answerQuestion();
+  } else {
+    return;
+  }
+};
+scoreBoard.addEventListener("click", highscores);
 start.addEventListener("click", startFunction);
-body.addEventListener("click", answerQuestion);
-// When I click the start button, it removes the current body and replaces it with a question and 4 options
+body.addEventListener("click", answerBoolean);
